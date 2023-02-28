f45f905e9b1e20ab34d130293072fdbb8df35d2d
ReetotpolKumar <reetotpolkumar@gmail.com>
2023-02-28 15:14:49 +0530

    final changes

---
+++ server/index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+
+const app = express()
+
+const mySQL = require('mysql')
+
+
+const db = mySQL.createPool(
+
+    {
+
+        host: 'localhost',
+
+        user: 'root',
+
+        password: '',
+
+        database: 'amazoncloneuserdetails'
+
+    }
+
+)
+
+
+const BodyParser = require('body-parser')
+
+const cors = require('cors')
+
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(BodyParser.urlencoded({ extended: true }))
+
+
+app.post("/api/NonQuery", (req, res) => {
+
+   let mySQL = req.body.mySQL
+
+   db.query(mySQL, (err, result) => {
+
+    console.log(err)
+
+    if (err == null)
+
+        res.send('Done')
+
+    else 
+
+        res.send('Error')
+
+  })
+
+});
+
+
+app.get ("/api/DataQuery", (req,res) => {
+
+    const mySQL = req.query.sql
+
+    db.query(mySQL, (err, result) => {
+
+        console.log(result)
+
+        res.send(result)
+
+    })
+
+}
+
+);
+
+app.delete ("/api/DeleteQuery", (req,res) => {
+
+    const mySQL = req.query.sql
+
+    db.query(mySQL, (err, result) => {
+
+        console.log(result)
+
+        res.send(result)
+
+    })
+
+}
+
+);
+
+
+app.listen(3001, () => {
+
+    console.log('Hello Hi MySQL Server')
+
+});
